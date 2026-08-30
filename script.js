07c4333f5cbb233d8d5bb942a3933495668f1ec1
Soham <brainosoham@gmail.com>
2026-08-30 08:17:42 +0530

    developed state machine for walk up walk down walk left walk right and made spritesheet, now going to implement movement

--- script.js
+++ script.js
@@ -18,7 +18,10 @@ let speed = 3
 let currentState
 
 function preload() {
-    this.load.image("player", "./assets/player.png");
+    this.load.spritesheet("player", "./assets/player-spritesheet.png", {
+        frameWidth: 16,
+        frameHeight: 16
+    });
 }
 
 function create() {
@@ -27,9 +30,47 @@ function create() {
     player = this.add.image(width / 2, height / 2, "player");
     player.setScale(10);
     cursors = this.input.keyboard.createCursorKeys();
+
+    this.anims.create({
+        key: "walk-left",
+        frames: this.anims.generateFrameNumbers("player", { start: 18, end: 23 }),
+        frameRate: 10,
+        repeat: -1
+    })
+
+    this.anims.create({
+        key: "walk-right",
+        frames: this.anims.generateFrameNumbers("player", { start: 12, end: 17 }),
+        frameRate: 10,
+        repeat: -1
+    })
+    
+    this.anims.create({
+        key: "walk-down",
+        frames: this.anims.generateFrameNumbers("player", { start: 0, end: 5 }),
+        frameRate: 10,
+        repeat: -1
+    })
+
+    this.anims.create({
+        key: "walk-up",
+        frames: this.anims.generateFrameNumbers("player", { start: 6, end: 11 }),
+        frameRate: 10,
+        repeat: -1
+    })
+
+    currentState = states.idle;
+    currentState.onEnter();
 }
 
 function update() {
+    const nextState = currentState.onUpdate();
+
+    if (nextState !== currentState) {
+        currentState.onExit();
+        currentState = states[nextState];
+        currentState.onEnter();
+    }
 }
 
 const states = {
@@ -37,27 +78,57 @@ const states = {
         onEnter() {
             player.anims.stop();
         },
-        onUpdate() {},
+        onUpdate() {
+            if (cursors.left.isDown) return "walk-left";
+            if (cursors.right.isDown) return "walk-right";
+            if (cursors.up.isDown) return "walk-up";
+            if (cursors.down.isDown) return "walk-down";
+            return "idle";
+        },
         onExit() {}
     },
     "walk-left": {
-        onEnter() {},
-        onUpdate() {},
+        onEnter() {
+            player.anims.play("walk-left", true);
+        },
+        onUpdate() {
+            if (!cursors.left.isDown) return "idle";
+            robot.x -= speed;
+            return "walk-left";
+        },
         onExit() {}
     },
     "walk-right": {
-        onEnter() {},
-        onUpdate() {},
+        onEnter() {
+            player.anims.play("walk-right", true);
+        },
+        onUpdate() {
+            if (!cursors.right.isDown) return "idle";
+            robot.x += speed;
+            return "walk-right";
+        },
         onExit() {}
     },
     "walk-up": {
-        onEnter() {},
-        onUpdate() {},
+        onEnter() {
+            player.anims.play("walk-up", true);
+        },
+        onUpdate() {
+            if (!cursors.up.isDown) return "idle";
+            robot.y -= speed;
+            return "walk-up";
+        },
         onExit() {}
     },
     "walk-down": {
-        onEnter() {},
-        onUpdate() {},
+        onEnter() {
+            player.anims.play("walk-down", true);
+        },
+        onUpdate() {
+            if (!cursors.down.isDown) return "idle";
+            robot.y += speed;
+            return "walk-down";
+        },
         onExit() {}
     }
 }
