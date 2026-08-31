485faf9ba2209cda186369cec6148e6eff2336f9
Soham <brainosoham@gmail.com>
2026-08-31 15:26:00 +0530

    developed jump and movement system using a much better setVelocity, comepletelry ditching state machine

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ const config = {
     physics: {
         default: "arcade",
         arcade: {
-            gravity: { y: 100 },
+            gravity: { y: 300 },
             debug: false
         }
     }
@@ -35,6 +35,8 @@ function preload() {
     });
 
     this.load.image("ground", "./assets/ground.png");
+
+    this.load.image("platform", "./assets/platform.png");
 }
 
 function create() {
@@ -43,17 +45,31 @@ function create() {
 
     ground = this.physics.add.staticGroup();
     ground.create(width / 2, height - 32, "ground").setScale(2).refreshBody();
+    
+    platform = this.physics.add.staticGroup();
+    platform.create(500, 500, "platform").setScale(0.5).refreshBody();
 
-    player = this.physics.add.sprite(width / 2, height / 2, "player").setScale(1.5);
+    player = this.physics.add.sprite(width / 2, 0, "player").setScale(1.25);
     player.body.setSize(player.width * 0.5, player.height * 0.7);
 
     this.physics.add.collider(player, ground);
+    this.physics.add.collider(player, platform);
 
     cursors = this.input.keyboard.createCursorKeys();
 }
 
 function update() {
+    if (cursors.space.isDown && player.body.touching.down) {
+        player.setVelocityY(-200);
+    }
 
+    if (cursors.left.isDown) {
+        player.setVelocityX(-160);
+    } else if (cursors.right.isDown) {
+        player.setVelocityX(160);
+    } else {
+        player.setVelocityX(0);
+    }
 }
 
 const game = new Phaser.Game(config);
